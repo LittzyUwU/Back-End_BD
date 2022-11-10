a6569151797971c503017bb747e69e4dfa02798c
LittzyUwU <gutierrezlittzy@gmail.com>
2022-11-10 17:58:46 -0600

    Agregar archivos de proyecto.

---
+++ Back-End_BD/Models/AlumnoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Back_End_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Back_End_BD.Models
+{
+    public class AlumnoDbContext : DbContext
+    {
+        private const string ConnectionString = "DefaultConnection";
+
+        public AlumnoDbContext() : base(ConnectionString)
+        {
+
+        }
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Profesor> Profesor { get; set; }
+    }
+}
